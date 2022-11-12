d4acfdbf75e7a471078320f3083f3e83293ae49e
Surajdhabale60 <112876188+Surajdhabale60@users.noreply.github.com>
2022-11-12 22:56:48 +0530

    Update Program.cs

--- LineComparison/LineComparison/Program.cs
+++ LineComparison/LineComparison/Program.cs
@@ -7,7 +7,7 @@ namespace LineComparison
         public static void Main(string[] args)
         {
             LineMain lineMain = new LineMain(1,2,3,4);
-            lineMain.Calculate();
+            lineMain.Length();
         }
     }
 }
